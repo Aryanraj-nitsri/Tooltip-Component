a9c4f12a4e5b945e9954771462f7e85766b56dcf
Aryan Raj <aryanraj123outlook@gmail.com>
2022-09-25 18:29:19 +0530

    unnecessary code deleted

--- src/Component/Tooltip.js
+++ src/Component/Tooltip.js
@@ -1,54 +1,44 @@
-import React, { useState } from 'react'
-import styled from "styled-components"
+import React  from "react";
+import styled from "styled-components";
 const Tool = styled.div`
-width:12%;
-background-color:black;
-position:absolute;
-color:white;
-padding:1rem;
-border-radius:5px;
-text-align:center;
-z-index:1;
-visibility:hidden;
+  width: 12%;
+  background-color: black;
+  position: absolute;
+  color: white;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  z-index: 1;
+  visibility: hidden;
 
-top:${({ top }) => top};
-left:${({left})=>left};
-&::before{
-    content:"";
-    position:absolute;
-    width:0;
-    border-top:10px solid transparent;
-    border-left:10px solid transparent;
-    border-right:10px solid black;
-    border-bottom:10px solid transparent;
-    top:35%;
-    left:-9%;
-
-}
-`
+  top: ${({ top }) => top};
+  left: ${({ left }) => left};
+  &::before {
+    content: "";
+    position: absolute;
+    width: 0;
+    border-top: 10px solid transparent;
+    border-left: 10px solid transparent;
+    border-right: 10px solid black;
+    border-bottom: 10px solid transparent;
+    top: 35%;
+    left: -9%;
+  }
+`;
 const Wrapper = styled.div`
-&:hover div{
-    visibility:visible;
-}
-`
+  &:hover div {
+    visibility: visible;
+  }
+`;
 
-export default function Tooltip({ children, message, top, left,style }) {
-   
+export default function Tooltip({ children, message, top, left, style }) {
 
-    const [visibility, setvisibility] = useState(false)
-    
-        return (
-        
-                <Wrapper style={style}>
-
-                    {children}
-                    <Tool top={top} left={left}>{message}</Tool>
-                
-                </Wrapper>
-
-                    
-                
-        
-        )
-    
+  return (
+    <Wrapper style={style}>
+      {children}
+      <Tool top={top} left={left}>
+        {message}
+      </Tool>
+    </Wrapper>
+  );
 }
