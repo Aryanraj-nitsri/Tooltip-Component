b6ee29f12e67d0cd31692e25e371032d8b69fa03
Aryan Raj <aryanraj123outlook@gmail.com>
2022-09-25 17:48:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Tooltip from "./Component/Tooltip";
 function App() {
+  const style = {
+    width: "10%",
+    margin: "20rem auto",
+    borderBottom: "2px dotted lightgray",
+    textAlign: "center",
+    fontSize: "1.2rem",
+    
+
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tooltip
+        message="Thanks for hovering! I am a tooltip"
+        top={"19rem"}
+        left={"54rem"}
+        style={style}
+      >
+        <p >hover over me</p>
+      </Tooltip>
     </div>
   );
 }
